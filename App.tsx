6fe7056b92699057b0ece3a677914015d2246879
cobytab <terra.andoh@gmail.com>
2021-07-23 11:06:37 +0000

    fix: :bug: Managed to fix env issue by renaming module
    Renamed module from @env to react-native-dotenv in the babel confige file

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 // env
-import { APP_NAME } from "@env";
+import { APP_NAME } from "react-native-dotenv";
 
 export default function App() {
   return (
