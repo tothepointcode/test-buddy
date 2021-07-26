7264c98f82077b03d791a8ed9375a90ccb7657d9
cobytab <terra.andoh@gmail.com>
2021-07-26 21:40:28 +0000

    feat: :construction: Styled the header

--- navigators/RootStack.tsx
+++ navigators/RootStack.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+// colors
+import { Colors } from "./../components/Colors";
+const { gray1, gray2, white, black } = Colors;
 
 import Home from "./../screens/Home";
 import Dashboard from "./../screens/Dashboard";
@@ -8,7 +11,13 @@ const Stack = createStackNavigator();
 
 const RootStack = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerTitleAlign: "center",
+        headerTintColor: white,
+        headerStyle: { backgroundColor: black },
+      }}
+    >
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Dashboard" component={Dashboard} />
     </Stack.Navigator>
